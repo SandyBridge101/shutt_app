04fbcdf19bf5fdf63683441aba7fe9b5f0281868
muizbello <bellomuiz78+1@gmail.com>
2025-02-13 01:13:59 +0000

    reverting to previouss version

--- lib/screens/signUp2.dart
+++ lib/screens/signUp2.dart
@@ -1,4 +1,4 @@
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:shutt_app/providers/authProvider.dart';
 import 'package:shutt_app/screens/signUp3.dart';
